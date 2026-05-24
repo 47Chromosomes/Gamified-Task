877ad2d56ba59a4f03873cc3b1e52b4aec8ac844
47Chromosomes <domingoalanranzc@gmail.com>
2026-05-24 21:28:29 +0800

    fixed init

--- mechanics/__init__.py
+++ mechanics/__init__.py
@@ -1,5 +1,5 @@
-from .algorithm import *
-from .data_structure import *
-from .menu import *
-from .player import *
-from .utils import *
+from .menu import Main_Menu
+from .player import Player
+from .data_structure import stack, queue, Task_HashTable
+from .system import Tasks_System
+from .utils import clear_screen, clear_screen, clear_lines
